edbf20bc6527fc6f03f10be179cc6d161def1b3e
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2019-12-02 15:10:46 -0800

    Deleted second copy of ReadMe.

--- server/index.js
+++ server/index.js
@@ -3,11 +3,37 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
+
+// MY OWN CODE:
+const db = require("./models");
+
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+
+
+
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+
+
+
+
+// MY OWN CODE:
+app.get("/associateattorney", (req, res) => {
+  db.AssociateAttorney.find({})
+    .then(dbAssociateAttorney => {
+      res.json(dbAssociateAttorney);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 
 
