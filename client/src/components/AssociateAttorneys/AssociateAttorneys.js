b61764379846fc8ad0f1796bacb40f55c0d9b455
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2019-12-03 13:29:21 -0800

    Comment reminders added to App.js file.  In AssociateAttorneys.js, attempt to use map method to find attorneys from db

--- client/src/components/AssociateAttorneys/AssociateAttorneys.js
+++ client/src/components/AssociateAttorneys/AssociateAttorneys.js
@@ -1,6 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import Card from "react-bootstrap/Card";
 
 
+function AssociateAttorneys() {
 
+    const [attorneys, setAttorneys] = useState([]);
 
-export default 
+    useEffect(() => {
+        API.getAttorneys().then(dbAttorneys => {
+            console.log(dbAttorneys);
+            setAttorneys(dbAttorneys);
+        })
+    }, []);
+
+    return (
+        <div>
+            {/* ??? How to input this information into a React-Bootstrap Card ??? */}
+                {attorneys.map(lawyer => {
+                    return (
+                        <Card>
+                            <Card.Img variant="top" src="" alt="generic picture" />
+                            <Card.Body>
+                                <Card.Title>{lawyer.name}</Card.Title>
+                                <Card.Text>
+                                    Practice={lawyer.practice}  
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    );
+                })}
+        </div>
+    )
+}
+
+export default AssociateAttorneys;
